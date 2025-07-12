f907084034a3a03d766c2a81098e6516e2b67c56
matthieu562 <matthieucachera@yahoo.fr>
2025-07-12 12:12:34 +0200

    test elo

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from chessdotcom import get_player_stats, Client
+
 
 app = Flask(__name__)
-
-
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    # Définir un User-Agent valide
+    Client.request_config['headers']['User-Agent'] = 'toto'
+
+    stats = get_player_stats("Lo_Chx").json
+    elo_rapid = stats['stats']['chess_rapid']['last']['rating']
+    #print("Elo rapid :", elo_rapid)
+    return f"Le ELO de Loïc est de {elo_rapid}"
+
+print(hello())
